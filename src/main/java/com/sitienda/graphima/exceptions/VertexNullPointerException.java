e5e771270672795bb838bdf9180a205c42fef662
comocomo <vasnikak@gmail.com>
2019-09-08 03:49:50 -0300

    VertexNullPointerException

---
+++ src/main/java/com/sitienda/graphima/exceptions/VertexNullPointerException.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2019, by Vasileios Nikakis
+ *
+ * graphima: yet another Java graph-theory library
+ */
+package com.sitienda.graphima.exceptions;
+
+/**
+ * VertexNullPointerException
+ *
+ * @author Vasileios Nikakis
+ */
+public class VertexNullPointerException extends RuntimeException {
+    
+    /**
+     * Constructor
+     */
+    public VertexNullPointerException() {
+        super("The vertex cannot contain null as data");
+    }
+
+    /**
+     * Constructor
+     * 
+     * @param string The exception message.
+     */
+    public VertexNullPointerException(String string) {
+        super(string);
+    }
+    
+}
