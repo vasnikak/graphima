38298d7c2c1843e8881f7d17c824f7921b24ba88
comocomo <vasnikak@gmail.com>
2019-09-07 21:40:30 -0400

    GraphDataMissingException

---
+++ src/main/java/com/sitienda/graphima/exceptions/GraphDataMissingException.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2019, by Vasileios Nikakis
+ *
+ * graphima: yet another Java graph-theory library
+ */
+package com.sitienda.graphima.exceptions;
+
+/**
+ * GraphDataMissingException
+ *
+ * @author Vasileios Nikakis
+ */
+public class GraphDataMissingException extends Exception {
+    
+    /**
+     * Constructor
+     */
+    public GraphDataMissingException() {
+        super("Graph type not found");
+    }
+
+    /**
+     * Constructor
+     * 
+     * @param string The exception message.
+     */
+    public GraphDataMissingException(String string) {
+        super(string);
+    }
+    
+}
