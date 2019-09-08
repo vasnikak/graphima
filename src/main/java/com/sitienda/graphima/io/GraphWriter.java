09106828016eef6e80ff1f59ec8989be8b35dfcd
comocomo <vasnikak@gmail.com>
2019-09-08 06:52:03 -0300

    Graph writer base class

--- src/main/java/com/sitienda/graphima/io/GraphWriter.java
+++ src/main/java/com/sitienda/graphima/io/GraphWriter.java
@@ -10,21 +10,23 @@ import com.sitienda.graphima.Graph;
 /**
  * The base class for each graph writer.
  * 
+ * @param <V> the type of objects that the graph contains
+ * 
  * @author Vasileios Nikakis
  */
-public abstract class GraphWriter {
+public abstract class GraphWriter<V> {
     
     /**
      * The graph.
      */
-    protected final Graph<?> graph;
+    protected final Graph<V> graph;
     
     /**
      * Constructor
      * 
      * @param graph the graph
      */
-    public GraphWriter(Graph<?> graph) { 
+    public GraphWriter(Graph<V> graph) { 
         this.graph = graph;
     }
     
