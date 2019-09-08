8460da11849ed978c74d0e74266567f7370dbf21
comocomo <vasnikak@gmail.com>
2019-09-08 06:49:00 -0300

    ErroneousFileFormatException

---
+++ src/main/java/com/sitienda/graphima/exceptions/ErroneousFileFormatException.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2019, by Vasileios Nikakis
+ *
+ * graphima: yet another Java graph-theory library
+ */
+package com.sitienda.graphima.exceptions;
+
+/**
+ * ErroneousFileFormatException
+ *
+ * @author Vasileios Nikakis
+ */
+public class ErroneousFileFormatException extends Exception {
+    
+    /**
+     * Constructor
+     */
+    public ErroneousFileFormatException() {
+        super("Erroneous file format");
+    }
+
+    /**
+     * Constructor
+     * 
+     * @param string The exception message.
+     */
+    public ErroneousFileFormatException(String string) {
+        super(string);
+    }
+    
+}
