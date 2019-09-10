27e7e1fe2f4db7cf71818dd4742e405878ee3408
comocomo <vasnikak@gmail.com>
2019-09-10 02:38:56 -0300

    Extends GraphFileReader

--- src/main/java/com/sitienda/graphima/io/GraphJSONReader.java
+++ src/main/java/com/sitienda/graphima/io/GraphJSONReader.java
@@ -6,7 +6,6 @@
 package com.sitienda.graphima.io;
 
 import com.sitienda.graphima.Graph;
-import com.sitienda.graphima.exceptions.ErroneousFileFormatException;
 import com.sitienda.graphima.exceptions.GraphDataMissingException;
 import com.sitienda.graphima.exceptions.GraphIOException;
 import com.sitienda.graphima.exceptions.VertexNotInGraphException;
@@ -27,7 +26,7 @@ public class GraphJSONReader<V> extends GraphFileReader<V> {
     /**
      * Constructor
      * 
-     * @param filepath the file path
+     * @param filepath the input file path
      */
     public GraphJSONReader(String filepath) {
         super(filepath);
@@ -37,18 +36,15 @@ public class GraphJSONReader<V> extends GraphFileReader<V> {
      * {@inheritDoc}
      */
     @Override
-    public Graph<V> readFile(Class<V> cls) throws GraphIOException, ErroneousFileFormatException {
+    public Graph<V> read(Class<V> cls) throws GraphIOException {
         try { 
             byte[] fileData = Files.readAllBytes(Paths.get(filepath));
             String json = new String(fileData,StandardCharsets.UTF_8);
             GraphJSONConverter<V> jsonConverter = new GraphJSONConverter<>();
             Graph<V> graph = jsonConverter.fromJson(json,cls);
             return graph;
-        }
-        catch (IOException e) { 
+        } catch (IOException | GraphDataMissingException | VertexNotInGraphException e) {
             throw new GraphIOException(e.getMessage());
-        } catch (GraphDataMissingException | VertexNotInGraphException e) {
-            throw new ErroneousFileFormatException(e.getMessage());
         }
     }
     
