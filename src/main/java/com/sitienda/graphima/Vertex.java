d39f82f61f747ce6b428d63c5e8ed07510508c90
comocomo <vasnikak@gmail.com>
2019-09-10 06:05:19 -0300

    Defined explicit generics

--- src/main/java/com/sitienda/graphima/Vertex.java
+++ src/main/java/com/sitienda/graphima/Vertex.java
@@ -13,19 +13,19 @@ import java.util.Objects;
  * The vertex of a graph.
  * Each vertex contains a set with all the outgoing edges.
  *
- * @param <T> the type of the objects that are encapsulated in each vertex
+ * @param <V> the type of the objects that are encapsulated in each vertex
  *
  * @author Vasileios Nikakis
  */
-public class Vertex<T> {
+public class Vertex<V> {
     /**
      * The contained object.
      */
-    private final T data;
+    private final V data;
     /**
      * The edges of the vertex.
      */
-    private final HashSet<Edge> edges;
+    private final HashSet<Edge<Vertex<V>>> edges;
     
     /**
      * Constructor
@@ -34,7 +34,7 @@ public class Vertex<T> {
      * 
      * @throws VertexNullPointerException if the data is null
      */
-    public Vertex(T data) throws VertexNullPointerException { 
+    public Vertex(V data) throws VertexNullPointerException { 
         if (data == null)
             throw new VertexNullPointerException("The vertex cannot contain null as data");
         this.data = data;
@@ -45,7 +45,7 @@ public class Vertex<T> {
      * 
      * @return the contained object.
      */
-    public T getData() { 
+    public V getData() { 
         return data;
     }
     
@@ -53,26 +53,30 @@ public class Vertex<T> {
      * 
      * @return the edges of the vertex.
      */
-    public HashSet<Edge> getEdges() { 
+    public HashSet<Edge<Vertex<V>>> getEdges() { 
         return edges;
     }
     
     /**
      * Checks if the contained data object is the same with the argument.
+     * 
      * @param data the item that needs to be checked.
+     * 
      * @return true or false.
      */
-    public boolean contains(T data) { 
+    public boolean contains(V data) { 
         return this.data.equals(data);
     }
     
     /**
      * Checks if the current vertex has an edge with another vertex.
+     * 
      * @param vertex the vertex that needs to be checked.
+     * 
      * @return true or false.
      */
-    public boolean hasEdgeWith(Vertex vertex) { 
-        for (Edge edge : edges) { 
+    public boolean hasEdgeWith(Vertex<V> vertex) { 
+        for (Edge<Vertex<V>> edge : edges) { 
             if (edge.containsVertex(vertex))
                 return true;
         }
@@ -81,21 +85,25 @@ public class Vertex<T> {
     
     /**
      * Checks if the current vertex has a specific edge.
+     * 
      * @param edge the edge that needs to be checked.
+     * 
      * @return true or false.
      */
-    public boolean hasEdge(Edge edge) { 
+    public boolean hasEdge(Edge<Vertex<V>> edge) { 
         return hasEdgeWith(edge.getVertex());
     }
     
     /**
      * Returns the corresponding edge with a specific edge or null if this
      * edge doesn't exist.
+     * 
      * @param vertex the edge to be found.
+     * 
      * @return true or false.
      */
-    public Edge getEdgeWith(Vertex vertex) { 
-        for (Edge edge: edges) { 
+    public Edge<Vertex<V>> getEdgeWith(Vertex<V> vertex) { 
+        for (Edge<Vertex<V>> edge: edges) { 
             if (edge.containsVertex(vertex))
                 return edge;
         }
@@ -104,29 +112,35 @@ public class Vertex<T> {
     
     /**
      * Adds an edge to the current vertex's set of edges.
+     * 
      * @param edge the edge to be added.
+     * 
      * @return true if the edge was added or false if the connection already exists.
      */
-    public boolean addEdge(Edge edge) { 
+    public boolean addEdge(Edge<Vertex<V>> edge) { 
         return edges.add(edge);
     }
     
     /**
      * Removes an edge from the current vertex's set of edges.
+     * 
      * @param edge the edge to be removed.
+     * 
      * @return true if the edge was in the edge, false otherwise.
      */
-    public boolean removeEdge(Edge edge) { 
+    public boolean removeEdge(Edge<Vertex<V>> edge) { 
         return edges.remove(edge);
     }
     
     /**
      * Removes an edge with a specific vertex.
+     * 
      * @param vertex the vertex that the corresponding edge will be removed.
+     * 
      * @return true if the connection was present, false otherwise.
      */
-    public boolean removeEdgeWith(Vertex vertex) { 
-        Edge edge = getEdgeWith(vertex);
+    public boolean removeEdgeWith(Vertex<V> vertex) { 
+        Edge<Vertex<V>> edge = getEdgeWith(vertex);
         if (edge != null) { 
             removeEdge(edge);
             return true;
@@ -136,17 +150,19 @@ public class Vertex<T> {
     
     /**
      * Returns a HashSet that contains the neighbor vertices.
+     * 
      * @return the neighbor vertices.
      */
-    public HashSet<Vertex<T>> getNeighbors() { 
-        HashSet<Vertex<T>> neighbors = new HashSet<>();
-        for (Edge edge : edges)
-            neighbors.add((Vertex<T>) edge.getVertex());
+    public HashSet<Vertex<V>> getNeighbors() { 
+        HashSet<Vertex<V>> neighbors = new HashSet<>();
+        for (Edge<Vertex<V>> edge : edges)
+            neighbors.add(edge.getVertex());
         return neighbors;
     }
     
     /**
      * Returns the number of neighbor vertices.
+     * 
      * @return the number of neighbor vertices.
      */
     public int getNeighborsSize() { 
