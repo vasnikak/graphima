3ddc9e82b9aa80e003492f9f095624001f2830b9
comocomo <vasnikak@gmail.com>
2019-09-10 06:05:40 -0300

    Defined explicit generics

--- src/main/java/com/sitienda/graphima/alg/BFSShortestPath.java
+++ src/main/java/com/sitienda/graphima/alg/BFSShortestPath.java
@@ -27,29 +27,29 @@ import java.util.Map;
  * object in case that a manual collision resolution is necessary.
  * </p>
  *
- * @param <T> the type of the objects that the graph contains
+ * @param <V> the type of the objects that the graph contains
  * 
  * @author Vasileios Nikakis
  */
-public class BFSShortestPath<T> extends GraphAlgorithm<T> {
+public class BFSShortestPath<V> extends GraphAlgorithm<V> {
     
     /**
      * Collision resolution comparator object.
      */
-    private final NodeComparator<T> collisionComp;
+    private final NodeComparator<V> collisionComp;
     
     /**
      * Inner helper class.
      * For each node, the parent node is stored.
      * 
-     * @param <V> each QueueItem encapsulates a Vertex
+     * @param <T> each QueueItem encapsulates a Vertex
      */
-    private class QueueItem<V extends Vertex<T>> { 
+    private class QueueItem<T extends Vertex<V>> { 
         
-        V node;
-        V parent;
+        T node;
+        T parent;
         
-        public QueueItem(V node, V parent) { 
+        public QueueItem(T node, T parent) { 
             this.node = node;
             this.parent = parent;
         }
@@ -61,7 +61,7 @@ public class BFSShortestPath<T> extends GraphAlgorithm<T> {
      * 
      * @param graph the graph
      */
-    public BFSShortestPath(Graph graph) {
+    public BFSShortestPath(Graph<V> graph) {
         super(graph);
         collisionComp = null;
     }
@@ -72,7 +72,7 @@ public class BFSShortestPath<T> extends GraphAlgorithm<T> {
      * @param graph the graph
      * @param collisionComp the node comparator to resolve any collisions
      */
-    public BFSShortestPath(Graph graph, NodeComparator<T> collisionComp) {
+    public BFSShortestPath(Graph<V> graph, NodeComparator<V> collisionComp) {
         super(graph);
         this.collisionComp = collisionComp;
     }
@@ -87,7 +87,7 @@ public class BFSShortestPath<T> extends GraphAlgorithm<T> {
      * 
      * @throws VertexNotInGraphException in case that any of the two vertices doesn't belong to the graph
      */
-    public Path findShortestPath(Vertex<T> start, Vertex<T> end) throws VertexNotInGraphException { 
+    public Path findShortestPath(Vertex<V> start, Vertex<V> end) throws VertexNotInGraphException { 
         // Both vertices have to exist inside the graph
         if (!graph.contains(start))
             throw new VertexNotInGraphException("The starting point vertex (" + start + ") doesn't exist in the graph");
@@ -99,20 +99,20 @@ public class BFSShortestPath<T> extends GraphAlgorithm<T> {
         execStats.reset();
         
         // We need a queue to store the nodes that wait to be examined
-        Deque<QueueItem<Vertex<T>>> queue = new ArrayDeque<>();
+        Deque<QueueItem<Vertex<V>>> queue = new ArrayDeque<>();
         // We need a map to be able to extract the path after the execution of the algorithm
-        Map<Vertex<T>,QueueItem<Vertex<T>>> visited = new HashMap<>();
+        Map<Vertex<V>,QueueItem<Vertex<V>>> visited = new HashMap<>();
         
         // Add the starting node in the queue
-        QueueItem<Vertex<T>> first = new QueueItem<>(start,null);
+        QueueItem<Vertex<V>> first = new QueueItem<>(start,null);
         queue.add(first);
         visited.put(start,first);
         // We will assign the destination queue item to this variable
-        QueueItem<Vertex<T>> target = null;
+        QueueItem<Vertex<V>> target = null;
         // While the queue is not empty
         while (!queue.isEmpty()) { 
             // Get the queue's first node
-            QueueItem<Vertex<T>> current = queue.pop();
+            QueueItem<Vertex<V>> current = queue.pop();
             // If it is the destination, stop the iteration
             if (current.node.equals(end)) { 
                 target = current;
@@ -120,11 +120,11 @@ public class BFSShortestPath<T> extends GraphAlgorithm<T> {
             }
             // If no comparator was defined for collision resolution
             if (collisionComp == null) { 
-                for (Edge edge : current.node.getEdges()) { 
+                for (Edge<Vertex<V>> edge : current.node.getEdges()) { 
                     // If we haven't visited yet the child node
                     if (visited.get(edge.getVertex()) == null) { 
                         // Add the node in the queue
-                        QueueItem<Vertex<T>> child = new QueueItem<>(edge.getVertex(),current.node);
+                        QueueItem<Vertex<V>> child = new QueueItem<>(edge.getVertex(),current.node);
                         queue.add(child);
                         // Mark the node as visited
                         visited.put(edge.getVertex(),child);
@@ -135,33 +135,33 @@ public class BFSShortestPath<T> extends GraphAlgorithm<T> {
             // If a comparator for collision resolution was defined
             } else { 
                 // Create a temporary list to resolve collisions
-                List<QueueItem<Vertex<T>>> children = new LinkedList<>();
-                for (Edge edge : current.node.getEdges()) { 
+                List<QueueItem<Vertex<V>>> children = new LinkedList<>();
+                for (Edge<Vertex<V>> edge : current.node.getEdges()) { 
                     // If we haven't visited yet the child node
                     if (visited.get(edge.getVertex()) == null) { 
                         // Add the node in the temporary list
-                        QueueItem<Vertex<T>> child = new QueueItem<>(edge.getVertex(),current.node);
+                        QueueItem<Vertex<V>> child = new QueueItem<>(edge.getVertex(),current.node);
                         children.add(child);
                         // Mark the node as visited
                         visited.put(edge.getVertex(),child);
                     }
                 }
                 // Sort the list using the collision resolution comparator
-                children.sort(new Comparator<QueueItem<Vertex<T>>>() {
+                children.sort(new Comparator<QueueItem<Vertex<V>>>() {
                     @Override
-                    public int compare(QueueItem<Vertex<T>> a, QueueItem<Vertex<T>> b) {
+                    public int compare(QueueItem<Vertex<V>> a, QueueItem<Vertex<V>> b) {
                         return collisionComp.compare(a.node.getData(),b.node.getData());
                     }
                 });
                 // Add the list items in the queue
-                for (QueueItem<Vertex<T>> child : children)
+                for (QueueItem<Vertex<V>> child : children)
                     queue.add(child);
             }
         }
         
         // Build the path from start to end
-        Path<Vertex<T>> path = new Path<>();
-        QueueItem<Vertex<T>> run = target;
+        Path<Vertex<V>> path = new Path<>();
+        QueueItem<Vertex<V>> run = target;
         while (run != null) { 
             path.prepend(run.node);
             run = visited.get(run.parent);
@@ -184,12 +184,12 @@ public class BFSShortestPath<T> extends GraphAlgorithm<T> {
      * 
      * @throws VertexNotInGraphException in case that any of the two vertices doesn't belong to the graph
      */
-    public Path findShortestPath(T start, T end) throws VertexNotInGraphException { 
+    public Path findShortestPath(V start, V end) throws VertexNotInGraphException { 
         // Find the corresponding vertices
-        Vertex<T> startVertex = graph.getVertexWithData(start);
+        Vertex<V> startVertex = graph.getVertexWithData(start);
         if (startVertex == null)
             throw new VertexNotInGraphException("The graph does not contain any vertex with data: " + start);
-        Vertex<T> endVertex = graph.getVertexWithData(end);
+        Vertex<V> endVertex = graph.getVertexWithData(end);
         if (endVertex == null)
             throw new VertexNotInGraphException("The graph does not contain any vertex with data: " + end);
         return findShortestPath(startVertex,endVertex);
