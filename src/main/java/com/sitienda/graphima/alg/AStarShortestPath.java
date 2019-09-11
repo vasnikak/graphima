40baaedfc9c345b22d85631cdc2b5dba7b6286fc
comocomo <vasnikak@gmail.com>
2019-09-11 08:38:36 -0300

    Defined the generics explicitly

--- src/main/java/com/sitienda/graphima/alg/AStarShortestPath.java
+++ src/main/java/com/sitienda/graphima/alg/AStarShortestPath.java
@@ -129,7 +129,7 @@ public class AStarShortestPath<V> extends GraphAlgorithm<V> {
      * 
      * @throws VertexNotInGraphException in case that any of the two vertices doesn't belong to the graph
      */
-    public Path findShortestPath(Vertex<V> start, Vertex<V> end) throws VertexNotInGraphException { 
+    public Path<Vertex<V>> findShortestPath(Vertex<V> start, Vertex<V> end) throws VertexNotInGraphException { 
         // Both vertices have to exist inside the graph
         if (!graph.contains(start))
             throw new VertexNotInGraphException("The starting point vertex (" + start + ") doesn't exist in the graph");
@@ -222,7 +222,7 @@ public class AStarShortestPath<V> extends GraphAlgorithm<V> {
      * 
      * @throws VertexNotInGraphException in case that any of the two vertices doesn't belong to the graph
      */
-    public Path findShortestPath(V start, V end) throws VertexNotInGraphException { 
+    public Path<Vertex<V>> findShortestPath(V start, V end) throws VertexNotInGraphException { 
         // Find the corresponding vertices
         Vertex<V> startVertex = graph.getVertexWithData(start);
         if (startVertex == null)
